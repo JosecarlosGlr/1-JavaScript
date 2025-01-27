14b44ea8632f690748178ae1eb1cdbd1e0a17aea
jose carlos <josecar400@gmail.com>
2025-01-27 21:21:42 +0100

    JavaScriptV1

---
+++ edad.js
@@ -0,0 +1,8 @@
+const mayoriaEdad =18;
+let edad = prompt ('Dime tu edad');
+if (edad<mayoriaEdad){
+    document.write("No puedes entrar en este sitio");
+} else{
+    document.write("<h2>Bienvenido, tenga cuidad</h2>");
+}
+document.write('Buen dia, es de buena educación decir "hola"');
